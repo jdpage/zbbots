0978e530c4b373de378324e71cbc37e91004ac88
Jonathan Page <jdp@jd-page.com>
2011-05-02 15:49:32 -0400

    probability oops

--- dice.rb
+++ dice.rb
@@ -5,14 +5,20 @@ class Dice
 	include Cinch::Plugin
 
 	match /roll (\d*)d(\d+)([+-]\d+)?/
-	prefix "?"
+	prefix /^\?/
 
 	def execute m, num, sides, diff
 		n = num.to_i
 		n = 1 if num == ""
 		d = sides.to_i
 		s = diff.to_i
-		sum = rand(d * n - n + 1) + n + s
+		# sum = rand(d * n - n + 1) + n + s
+		if n > 50
+			m.reply "only rolling 50 dice"
+			n = 50
+		end
+		sum = s
+		n.times { sum += rand(d) + 1 }
 		m.reply "#{m.user.nick} rolls a #{sum}!"
 	end
 end
